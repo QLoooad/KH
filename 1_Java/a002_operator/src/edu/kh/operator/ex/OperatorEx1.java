1575c9edf6ea537315df43f151be68b04f3f876f
QLoooad <ckcc0813@gmail.com>
2023-02-08 12:38:55 +0900

    230208_산술연산자

--- 1_Java/a002_operator/src/edu/kh/operator/ex/OperatorEx1.java
+++ 1_Java/a002_operator/src/edu/kh/operator/ex/OperatorEx1.java
@@ -8,6 +8,25 @@ public class OperatorEx1 {
 
 		Scanner sc = new Scanner(System.in);
 		
+		//산술연산자 + - * / %(modulo, mod) 
+		
+		System.out.println("두 정수를 입력받아 산술 연산 결과 출력");
+		
+		System.out.println("정수 입력 1 : ");
+		int input1 = sc.nextInt();
+		
+		System.out.println("정수 입력 2 : ");
+		int input2 = sc.nextInt();
+		
+		System.out.printf("%d + %d = %d \n", input1, input2, input1 + input2);
+		
+		System.out.printf("%d - %d = %d \n", input1, input2, input1 - input2);
+		
+		System.out.printf("%d * %d = %d \n", input1, input2, input1 * input2);
+		
+		System.out.printf("%d / %d = %d \n", input1, input2, input1 / input2);
+		
+		System.out.printf("%d %% %d = %d \n", input1, input2, input1 % input2);
 	}
 
 }
