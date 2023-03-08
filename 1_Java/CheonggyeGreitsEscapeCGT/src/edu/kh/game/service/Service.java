9bf3336414c72c78557ee2058d9da4f88ea0986d
QLoooad <ckcc0813@gmail.com>
2023-03-08 17:50:31 +0900

    230308

--- 1_Java/CheonggyeGreitsEscapeCGT/src/edu/kh/game/service/Service.java
+++ 1_Java/CheonggyeGreitsEscapeCGT/src/edu/kh/game/service/Service.java
@@ -1,6 +1,8 @@
 package edu.kh.game.service;
 
 
+import java.util.Random;
+
 import edu.kh.game.dto.Player;
 
 public class Service {
@@ -64,6 +66,31 @@ public class Service {
 		return p1.getDefense();
 	}
 	
+	
+	Random random = new Random();
+	
+	public int whatItem() {
+		int whatItem = random.nextInt(100) + 1;
+		int getItemRandom = random.nextInt(100) + 1;
+		
+		if(getItemRandom >= 1 && getItemRandom <= 40) {//40퍼 확률 아이템 얻기
+			
+			if(whatItem >= 1 && whatItem <= 15) {//의료상자
+				return 1;
+			}else if(whatItem >= 16 && whatItem <= 35) {//붕대
+				return 2;
+			}else if(whatItem >= 36 && whatItem <= 55) {//커피
+				return 3;
+			}else if(whatItem >= 56 && whatItem <= 77) {//빵
+				return 4;
+			}else {//컵라면
+				return 5;
+			}
+		}else {
+			return 0;
+		}
+	}
+	
 	//아이템 랜덤값 들어오면 해당 아이템을 null배열 or "" 에 넣어주기
 	public void addItem(int itemNum) {
 		String item = "";
@@ -85,9 +112,14 @@ public class Service {
 				}
 			}
 		}
+		if(itemNum != 0) {
+			System.out.printf("[%d 을/를 얻었습니다.]", item);
+		}else {
+			System.out.println("[아이템을 찾지 못했습니다.]");
+		}
 		
-		System.out.printf("%d을/를 얻었습니다.", item);
 	}
+	
 	
 	public void useItem(int itemNum) {//해당인덱스 1~5까지 고르면 사용
 		itemNum -= 1;
