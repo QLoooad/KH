25e3ea6728755dea008112f662d6ad1a9195d95b
QLoooad <ckcc0813@gmail.com>
2023-03-12 18:10:37 +0900

    완?성(탕비실 중복)

--- 1_Java/CheonggyeGreitsEscape123/src/edu/kh/game/view/GameView.java
+++ 1_Java/CheonggyeGreitsEscape123/src/edu/kh/game/view/GameView.java
@@ -7,7 +7,6 @@ import edu.kh.game.dto.*;
 
 public class GameView {
 	static Scanner sc = new Scanner(System.in);
-	public static String name;
 	public static Service gameService = new Service();
 	public static Building floor = new Building();
 
@@ -34,8 +33,7 @@ public class GameView {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("[플레이어 이름을 지어주세요.]");
 		System.out.print("이름: ");
-		name = sc.next();
-		Player p1 = new Player(name);
+		gameService.setName(sc.next());
 	}
 
 	// 시작메뉴 화면
@@ -68,7 +66,7 @@ public class GameView {
 				clearScreen();
 				playerNameSet(); /* 플레이어 이름 짓기 */
 				clearScreen();
-				System.out.printf("[%s님, 게임을 시작합니다.]\n", name);
+				System.out.printf("[%s님, 게임을 시작합니다.]\n", gameService.getName());
 				for (int i = 3; i >= 1; i--) {
 					try {
 						System.out.print(i + "     ");
@@ -120,41 +118,72 @@ public class GameView {
 
 	// 게임 인트로
 	public void displayIntro() {
-		System.out.println("●평화로운 청계 그레이츠 빌딩 옥상정원●");
+		System.out.println("  나는 남들과 다름없는 학생이었다.");
 		enter();
-		System.out.println("  \"정말 기분 좋은 오후야~\"");
+		System.out.println("  그날이 오기 전까지는.......");
 		enter();
-		System.out.println("  \"그러게 정말 오늘 날씨도 맑고 정말 상쾌하다!\"");
+		System.out.println("  --------------------1일 전--------------------");
 		enter();
-		System.out.println("  \"그런데 어디선가 이상한 소리가 들리지 않아?\"");
+		System.out.println("  오늘도 다름없이 혼자 점심을 먹고 쏟아지는 잠을 참으며 수업을 듣던 그때");
 		enter();
-		System.out.println("  \"응? 이상한 소리?\"");
+		System.out.println("  어디선가 들리는 귀가 찢어질듯한 폭발음과 충돌음,\n  그리고 멀리서 작게 들리는 것 같은 비명소리");
 		enter();
-		System.out.println("  \"(비명소리) 끄아아악!\"");
+		System.out.println("  모두 놀란 듯 강의실 뒤쪽 창문으로 가 밖의 상황을 살폈다.");
 		enter();
-		System.out.println("  \"뭐지?\"");
+		System.out.println("  차들은 서로 뒤엉켜있었고 사람들은 서로를 공격하거나 도망치고 있었다.");
 		enter();
-		System.out.println("  \"(안내방송) 비상, 비상. 빌딩 내부에 있으신 분은\n" + "  모두 빌딩 밖으로 대피해주시기 바랍니다.\"");
+		System.out.println("  많이 이상함을 느낀 나는 바로 가방을 챙기고 비상계단으로 달려갔다.\n" + "  이미 수업은 내 알 바가 아니었다.");
 		enter();
-		System.out.println("  \"으악!?\"");
+		System.out.println("  비상계단 문을 여는 순간 1층에서 올라오는 피 칠갑이 된 사람 하나가 이미 다른 사람을 붙잡고 있었다.");
 		enter();
-		System.out.println("  \"???\"");
+		System.out.println("  주춤하던 사이 나는 붙잡힌 사람과 눈이 마주쳤고\n" + "  별다른 대응방법이 없었던 난 그 사람을 뒤로하고 위층으로 올라갈 수밖에 없었다.");
 		enter();
-		System.out.println("  \"좀.. 좀비다!\"");
+		System.out.println("  그리고 들려오는 살려달라는 비명과 함께 작아지는소리........");
 		enter();
-		System.out.println("  \"모두 뛰어!\"");
+		System.out.println("  옥상에 올라온 후 밖은 비명소리로 가득하다.\n"
+						+  "  아무도 올라오지 않은 것을 보면 모두 그 사람과 같은 운명이 되었으리라 짐작이 간다.");
 		enter();
-		System.out.printf("●이윽고 %s은(는) 옥상정원 구석으로\n" + "  대피에 성공한다.●\n", name);
+		System.out.println("  이것이 내가 옥상에 올라오기 전까지 일이다.");
 		enter();
-		System.out.printf("●하지만 %s은(는) 이 많은 좀비를 뚫고\n" + "  1층까지 무사히 도달할 수 있을까?●", name);
+		System.out.println("  홀로 옥상에 올라온 나는 가방에 있던 물과 젤리로 하루를 버텼다.");
 		enter();
+		System.out.println("  아직 여유는 있지만 더 오래 버틸 수 없다는 건 알고 있다.");
+		enter();
+		System.out.println("  하루밖에 지나지 않았지만 이젠 내려가야 한다. 아니면 여기서 굶어죽던가");
+		enter();
+		System.out.println("  아무것도 하지 않고 죽는 것보다야 뭐라도 해보고 죽는 게 좋을 것 같다.");
+		enter();
+		System.out.println("  이제 나는 내려가기 위한 옥상 문을 연다.");
+		enter();
+		
 		clearScreen();
 		displayGame();
 
 	}
 
+	public static void displayOutro() {
+		System.out.println("  나는 힘겹게 로비에 있던 좀비를 처리할 수 있었다.");
+		enter();
+		System.out.println("  몸은 이미 만신창이다. 옷은 찢어져있고 몸에서는 피비린내가 풍긴다.");
+		enter();
+		System.out.println("  손가락 하나 까딱하기도 힘든 몸으로 겨우 일어나 로비를 살펴봤지만\n"
+						+  "  나와 내가 죽인 좀비 말고는 아무도 없다.");
+		enter();
+		System.out.println("  나는 긴장이 풀려 지쳐 쓰러지지만, 살아있다는 것에 안도한다.");
+		enter();
+		System.out.println("  현 사태는 끝나지 않았고, 나는 겨우 학원 건물을 탈출했을 뿐이다.");
+		enter();
+		System.out.println("  이제 시작이다.");
+		enter();
+		clearScreen();
+		System.out.println("[게임 끝]");
+		enter();
+		clearScreen();
+		displayEnding(); /* 엔딩 */
+	}
+
 	public void displayGame() {
-		
+
 		int input = 0;
 		String straight = "┃";
 		while (viewFloor() > 1) {
@@ -164,7 +193,7 @@ public class GameView {
 			} else {
 				System.out.println();
 				System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-				System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+				System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 				System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 				System.out.println("                                     ");
 				System.out.printf("           " + floor.getRoom(viewFloor() - 1));
@@ -180,7 +209,6 @@ public class GameView {
 
 				input = sc.nextInt();
 				sc.nextLine(); // 입력 버퍼 개행문자 제거
-				
 
 				switch (input) {
 				case 0:
@@ -193,12 +221,12 @@ public class GameView {
 						break;
 					}
 					gameService.moveFloor(true);
-					
+
 					break;
 				case 1:
 					clearScreen();
 					checkingRoom(0);
-					
+
 					break;
 				case 2:
 					checkingRoom(1);
@@ -239,7 +267,7 @@ public class GameView {
 			System.out.println();
 			System.out.println();
 			System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-			System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+			System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 			System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 			System.out.println("                                     ");
 			System.out.println("    이미 들어간 방이거나                 ");
@@ -282,7 +310,7 @@ public class GameView {
 			System.out.println();
 			System.out.println();
 			System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-			System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+			System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 			System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 			System.out.println("    나이스!                          ");
 			System.out.println("    키를 획득하였습니다.                ");
@@ -332,7 +360,7 @@ public class GameView {
 			System.out.println();
 			System.out.println();
 			System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-			System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+			System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 			System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 			System.out.println("                                     ");
 			System.out.println("    키 없음                ");
@@ -378,7 +406,7 @@ public class GameView {
 			System.out.println();
 			System.out.println();
 			System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-			System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+			System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 			System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 			System.out.println("                                     ");
 			System.out.println("    키를 사용하여 내려감               ");
@@ -420,7 +448,7 @@ public class GameView {
 				System.out.println();
 				System.out.println();
 				System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-				System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+				System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 				System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 				System.out.println("                                     ");
 				System.out.println("    좀비가 나타났습니다.                ");
@@ -462,7 +490,7 @@ public class GameView {
 					System.out.println();
 					System.out.println();
 					System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-					System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(),
+					System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(),
 							viewFloor());
 					System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 					System.out.println("                                     ");
@@ -513,7 +541,7 @@ public class GameView {
 			System.out.println();
 			System.out.println();
 			System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-			System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+			System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 			System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 			System.out.println("                                     ");
 			System.out.println("    이제 나가기만 하면 된다.               ");
@@ -555,7 +583,7 @@ public class GameView {
 			System.out.println();
 			System.out.println();
 			System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-			System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+			System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 			System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 			System.out.println("                                     ");
 			System.out.println("    여기서 도망친다면 탈출 할 수                 ");
@@ -596,7 +624,7 @@ public class GameView {
 			System.out.println();
 			System.out.println();
 			System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-			System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+			System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 			System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 			System.out.println("                                     ");
 			System.out.println("    도망에 성공하였습니다.                 ");
@@ -637,7 +665,7 @@ public class GameView {
 			System.out.println();
 			System.out.println();
 			System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-			System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+			System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 			System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 			System.out.println("    나가기 직전 건물의 수호자      ");
 			System.out.println("    경비 좀비가 나타났습니다.                 ");
@@ -684,7 +712,7 @@ public class GameView {
 			System.out.println();
 			System.out.println();
 			System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-			System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+			System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 			System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 			System.out.println("                                     ");
 			System.out.println("    경비 좀비는 타격을 입었지만                   ");
@@ -731,7 +759,7 @@ public class GameView {
 			System.out.println();
 			System.out.println();
 			System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-			System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+			System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 			System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 			System.out.println("                                     ");
 			System.out.println("    누적된 피해로 인해                  ");
@@ -778,7 +806,7 @@ public class GameView {
 			System.out.println();
 			System.out.println();
 			System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-			System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+			System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 			System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 			System.out.println("                                     ");
 			System.out.println("    당신은 죽었습니다.                  ");
@@ -801,10 +829,9 @@ public class GameView {
 			System.out.println();
 
 			switch (input) {
-			case 1: displayMenu();
-			
-			
-			
+			case 1:
+				displayMenu();
+
 			default:
 				System.out.println("[메뉴에 존재하는 번호만 입력 해주세요]");
 			}
@@ -819,7 +846,7 @@ public class GameView {
 			System.out.println();
 			System.out.println();
 			System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-			System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+			System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 			System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 			System.out.println("                                     ");
 			System.out.println("    경비 좀비를 처리했습니다.                  ");
@@ -844,13 +871,36 @@ public class GameView {
 			switch (input) {
 			case 1:
 
-				// ending();
+				displayOutro();
 				break;
 
 			default:
 				System.out.println("[메뉴에 존재하는 번호만 입력 해주세요]");
 			}
 		}
+	}
+	// 게임 아웃트로
+
+	// 엔딩
+	public static void displayEnding() {
+		System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+		System.out.println("┃                End                 ┃");
+		System.out.println("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+		System.out.println("  만든 이:");
+		System.out.println("    정용화 / 박종욱 / 장반석");
+		System.out.println("    정송이 / 최근태 / 이상준");
+		clearScreen();
+		System.out.println("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+		System.out.println("┃====================================┃");
+		System.out.println("┃      Cheong-gye Greits Escape      ┃");
+		System.out.println("┃====================================┃");
+		System.out.println("┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫");
+		System.out.println("┃  /     /       / /   /     /   /   ┃");
+		System.out.println("┃   /  /    THANKS TO PLAY! /  /    /┃");
+		System.out.println("┃    / /      /        /      /   /  ┃");
+		System.out.println("┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫");
+		System.out.println("┃                                    ┃");
+		System.out.println("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
 	}
 
 ///////////////////////////////////////////////////////////////////////////////////////
@@ -868,7 +918,7 @@ public class GameView {
 					System.out.println();
 					System.out.println();
 					System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-					System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(),
+					System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(),
 							viewFloor());
 					System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 					System.out.println("                                     ");
@@ -919,7 +969,7 @@ public class GameView {
 				System.out.println();
 				System.out.println();
 				System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-				System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+				System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 				System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 				System.out.println("                                     ");
 				System.out.println("    재빠르게 회피하고 좀비를                ");
@@ -957,7 +1007,7 @@ public class GameView {
 				System.out.println();
 				System.out.println();
 				System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-				System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+				System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 				System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 				System.out.println("                                     ");
 				System.out.println("    공격을 당했지만                ");
@@ -1002,7 +1052,7 @@ public class GameView {
 				System.out.println();
 				System.out.println();
 				System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-				System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+				System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 				System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 				System.out.println("                                     ");
 				System.out.println("    재빠르게 회피하고                 ");
@@ -1040,7 +1090,7 @@ public class GameView {
 				System.out.println();
 				System.out.println();
 				System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-				System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+				System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 				System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 				System.out.println("                                     ");
 				System.out.println("    서로 공격을 주고 받았습니다.                ");
@@ -1084,7 +1134,7 @@ public class GameView {
 		System.out.println();
 		System.out.println();
 		System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-		System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+		System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 		System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 		System.out.println("    사용할 아이템을 선택해 주세요.                          ");
 		gameService.itemView();
@@ -1140,7 +1190,7 @@ public class GameView {
 			System.out.println();
 			System.out.println();
 			System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-			System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+			System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 			System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 			System.out.println("                                     ");
 			System.out.println("          도망에 실패했습니다.                ");
@@ -1180,14 +1230,14 @@ public class GameView {
 
 	/////// 탕비실
 	public void tang() {
-		
+
 		int input = 0;
 		while (input != 1) {
 			String straight = "┃";
 			System.out.println();
 			System.out.println();
 			System.out.println("┏━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━┓");
-			System.out.printf("┃HP : %d/5┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
+			System.out.printf("┃HP : %d/3┃STN : %d/3   ┃현재 위치 : %d F ┃\n", viewHp(), viewStamina(), viewFloor());
 			System.out.println("┃━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┫");
 			System.out.println("                                     ");
 			System.out.println("    탕비실에서 체력과 스태미너를                ");
@@ -1269,6 +1319,14 @@ public class GameView {
 		}
 	}
 
+	public String getName() {// 저장된 이름 얻기
+		return gameService.getName();
+	}
+
+	public void itemView() {// 저장된 아이템 출력
+		gameService.itemView();
+	}
+
 	private boolean checkKey() {
 		if (gameService.isKey() == true) {
 			yesKey();
@@ -1309,7 +1367,7 @@ public class GameView {
 	}
 
 	public void useItem(int index) { // 사용하면 삭제"null"로 변경
-		gameService.useItem(index - 1);
+		gameService.useItem(index);
 	}
 
 }
