55742e3adeed0b300a28abdbc64ae6ba1a918fad
QLoooad <ckcc0813@gmail.com>
2023-05-18 21:11:04 +0900

    230518_PingPong

--- PingPong/PingPong/src/main/java/com/pingpong/project/mypage/controller/MypageController.java
+++ PingPong/PingPong/src/main/java/com/pingpong/project/mypage/controller/MypageController.java
@@ -24,12 +24,13 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.pingpong.project.board.model.dto.Board;
+import com.pingpong.project.common.utility.Util;
 import com.pingpong.project.member.model.dto.Member;
 import com.pingpong.project.mypage.model.dto.MyPage;
 import com.pingpong.project.mypage.model.service.MypageService;
 
 
-@SessionAttributes({"loginMember"})
+@SessionAttributes({"loginMember", "mypage"})
 @RequestMapping("/mypage")
 @Controller
 public class MypageController {
@@ -44,10 +45,10 @@ public class MypageController {
 			, Model model) {
 		
 		
-		MyPage mypage = service.selectMemberProfile(memberNo);
-		List<Board> boardList = service.selectBoardList(memberNo);
-		List<Board> boardMarkList = service.selectBoardMarkList(memberNo);
-		List<Board> boardLikeList = service.selectBoardLikeList(memberNo);
+		MyPage mypage = service.selectMemberProfile(memberNo);  // 멤버 프로필 회원의 정보
+		List<Board> boardList = service.selectBoardList(memberNo);  // 게시글 목록
+		List<Board> boardMarkList = service.selectBoardMarkList(memberNo);  // 북마크한 게시글 목록
+		List<Board> boardLikeList = service.selectBoardLikeList(memberNo);  // 좋아요한 게시글 목록
 		
 		model.addAttribute("mypage", mypage);
 		model.addAttribute("boardList", boardList);
@@ -60,48 +61,57 @@ public class MypageController {
 	// 내 정보 수정으로 이동
 	@GetMapping("/myPageModi")
 	public String myPageModi(@SessionAttribute("loginMember") Member loginMember, Model model) {
-		
-		
-		
 		return "personal/myPageModi"; 
 	}
 	
 	
-	// 회원 정보 수정
-	@PostMapping("/myPageModi")
-	public String updateInfo(Member updateMember
-						, @SessionAttribute("loginMember") Member loginMember
-						, RedirectAttributes ra) {
-		
-		updateMember.setMemberNo(loginMember.getMemberNo()); // 로그인한 회원의 번호를 updateMember에 추가
-		
-//		System.out.println(updateMember.getMemberNickname());
-//		System.out.println(updateMember.getMemberUrl());
-//		System.out.println(updateMember.getMemberNo());
-		
-		int result = service.updateInfo(updateMember);
-
-//		System.out.println("result : " + result);
-		String message = null;
-		
-		if(result > 0) { // 성공	
-			message = "회원 정보가 수정되었습니다.";
-			
-			// Session에 로그인 된 회원 정보 수정
-			loginMember.setMemberNickname(updateMember.getMemberNickname());
-			loginMember.setMemberUrl(updateMember.getMemberUrl());
-			
-		}else { // 실패		
-			message = "회원 정보 수정 실패";		
-		}
-		
-		System.out.println(message);
-		
-		ra.addFlashAttribute("message", message);
-		
-		return "redirect:myPageModi";
-	}
 	
+	// 내 정보 편집
+	@PostMapping("/myPageModi")
+	public String updateInfoAndProfile(Member updateMember
+									, @RequestParam(value="profileImage", required=false) MultipartFile profileImage
+									, @SessionAttribute("loginMember") Member loginMember
+									, @SessionAttribute("mypage") MyPage mypage
+									, RedirectAttributes ra
+									, HttpSession session) throws IllegalStateException, IOException {
+		
+	    updateMember.setMemberNo(loginMember.getMemberNo());
+
+	    // 닉네임, url 수정
+	    int infoResult = service.updateInfo(updateMember);
+
+	    String webPath = "/resources/images/profileImage/";
+	    
+	    String filePath = session.getServletContext().getRealPath(webPath);
+	    System.out.println(profileImage);
+	    
+	    String fileName = profileImage.getOriginalFilename();
+		System.out.println(fileName);
+		String reName = Util.fileRename(fileName);
+		System.out.println(reName);
+	    // 프로필 이미지 수정
+	    int profileResult = service.updateProfile(profileImage, reName, webPath, filePath, loginMember.getMemberNo());
+
+	    String message = null;
+	    if (infoResult > 0 && profileResult > 0) {
+	        message = "회원 정보가 수정되었습니다.";
+
+	        // Session에 로그인 된 회원 정보 수정
+	        loginMember.setMemberNickname(updateMember.getMemberNickname());
+	        loginMember.setMemberUrl(updateMember.getMemberUrl());
+	        
+	        mypage.setProfileImage(webPath+reName);
+	        
+	    } else {
+	        message = "회원 정보 수정 실패";
+	    }
+
+	    ra.addFlashAttribute("message", message);
+
+	    return "redirect:myPageModi";
+	}
+
+
 	
 	// 비밀번호 수정
 	@PostMapping("/changePw")
@@ -155,15 +165,16 @@ public class MypageController {
 			status.setComplete(); // 로그아웃
 			
 			// 쿠키 삭제
-//			Cookie cookie = new Cookie("saveId", "");
-//			cookie.setMaxAge(0);
-//			cookie.setPath("/");
+			Cookie cookie = new Cookie("saveId", "");
+			cookie.setMaxAge(0);
+			cookie.setPath("/");
+			resp.addCookie(cookie);
 		}		
 
 		// 탈퇴 실패
 		else {
 			message = "현재 비밀번호가 일치하지 않습니다";
-			path += "myPageModi";
+			path += "changePw";
 		}
 		
 		ra.addFlashAttribute("message", message);
@@ -172,31 +183,6 @@ public class MypageController {
 	}
 	
 	
-	// 프로필 이미지 수정
-	@PostMapping("/profile")
-	public String updateProfile(@RequestParam(value="profileImage", required=false) MultipartFile profileImage 
-							, @SessionAttribute("loginMember") Member loginMember
-							, RedirectAttributes ra 
-							, HttpSession session
-							) throws IllegalStateException, IOException{
-		
-		// 웹 접근 경로
-		String webPath = "/resources/images/member/";
-		
-		// 실제로 이미지 파일이 저장되어야 하는 서버 컴퓨터 경로
-		String filePath = session.getServletContext().getRealPath(webPath);
-		
-		// 프로필 이미지 수정 서비스 호출
-		int result = service.updateProfile(profileImage, webPath, filePath, loginMember.getMemberNo());
-		
-		String message = null;
-		if(result > 0) message = "프로필 이미지가 변경되었습니다";
-		else		   message = "프로필 변경 실패";
-		
-		ra.addFlashAttribute("message", message);
-		
-		return "redirect:profile";
-	}
 	
     
     // 배경화면 변경
--- PingPong/PingPong/src/main/java/com/pingpong/project/search/controller/SearchController.java
+++ PingPong/PingPong/src/main/java/com/pingpong/project/search/controller/SearchController.java
@@ -1,5 +1,8 @@
 package com.pingpong.project.search.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class SearchController {
 
 }
