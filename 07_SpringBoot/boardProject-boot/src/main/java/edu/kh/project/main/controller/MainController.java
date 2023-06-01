863289122509bbec841ae0581664b0d609f50344
QLoooad <ckcc0813@gmail.com>
2023-06-01 11:42:27 +0900

    230601_SpringBoot

---
+++ 07_SpringBoot/boardProject-boot/src/main/java/edu/kh/project/main/controller/MainController.java
@@ -0,0 +1,22 @@
+package edu.kh.project.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/") 
+	public String mainForword(Model model) {
+		
+		model.addAttribute("message", "스프링 부트 테스트");
+		
+		// Spring MVC : /webapp/WEB-INF/views/common/main.jsp
+		
+		
+		// Spring Boot(+thumelaef 템플릿 엔진)
+		// src/main/resources/templates/common/main.html
+		return "common/main";
+	}
+}
